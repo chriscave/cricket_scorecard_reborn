14a9b1c28b99982247bfa0728a942d5b20647cb3
Chris <chriscave89@gmail.com>
2023-01-23 19:29:25 +0100

    refactor test

--- test/test_match.py
+++ test/test_match.py
@@ -5,41 +5,37 @@ from scorecard import Match, Player
 
 
 class TestMatch(unittest.TestCase):
+    def setUp(self):
+        self.team1 = [Player(f"1-{i}") for i in range(11)]
+        self.team1_name = "A"
+        self.team2 = [Player(f"2-{i}") for i in range(11)]
+        self.team2_name = "B"
+        self.match = Match(self.team1, self.team1_name, self.team2, self.team2_name)
+
     def test_add_ball_new_inns(self):
-        team1 = [Player(f"1-{i}") for i in range(11)]
-        team1_name = "A"
-        team2 = [Player(f"2-{i}") for i in range(11)]
-        team2_name = "B"
-        match = Match(team1, team1_name, team2, team2_name)
 
-        match.add_ball(team1[0], "1")
+        self.match.add_ball(self.team1[0], "1")
 
-        actual = match.team2_bat_inns
-        expected = [{team1[0]: ["1"]}]
+        actual = self.match.team2_bat_inns
+        expected = [{self.team1[0]: ["1"]}]
         self.assertEqual(actual, expected)
 
     def test_add_ball_new_over(self):
-        team1 = [Player(f"1-{i}") for i in range(11)]
-        team1_name = "A"
-        team2 = [Player(f"2-{i}") for i in range(11)]
-        team2_name = "B"
-        match = Match(team1, team1_name, team2, team2_name)
 
-        match.team2_bat_inns = [{team1[0]: ["0", "0", "0", "0", "0", "0"]}]
-        match.add_ball(team1[1], "4")
-        actual = match.team2_bat_inns
-        expected = [{team1[0]: ["0", "0", "0", "0", "0", "0"]}, {team1[1]: ["4"]}]
+        self.match.team2_bat_inns = [{self.team1[0]: ["0", "0", "0", "0", "0", "0"]}]
+        self.match.add_ball(self.team1[1], "4")
+        actual = self.match.team2_bat_inns
+        expected = [
+            {self.team1[0]: ["0", "0", "0", "0", "0", "0"]},
+            {self.team1[1]: ["4"]},
+        ]
         self.assertEqual(actual, expected)
 
     def test_add_ball_current_over(self):
-        team1 = [Player(f"1-{i}") for i in range(11)]
-        team1_name = "A"
-        team2 = [Player(f"2-{i}") for i in range(11)]
-        team2_name = "B"
-        match = Match(team1, team1_name, team2, team2_name)
-
-        match.team2_bat_inns = [{team1[0]: ["0", "0", "0"]}]
-        match.add_ball(team1[0], "4")
-        actual = match.team2_bat_inns
-        expected = [{team1[0]: ["0", "0", "0", "4"]}]
+        self.match.team2_bat_inns = [{self.team1[0]: ["0", "0", "0"]}]
+        self.match.add_ball(self.team1[0], "4")
+        actual = self.match.team2_bat_inns
+        expected = [{self.team1[0]: ["0", "0", "0", "4"]}]
         self.assertEqual(actual, expected)
+
+    # def test_add_ball_new_bowler_unfinished_over(self):
