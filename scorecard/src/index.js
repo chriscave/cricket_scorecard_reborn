215e5ce2ccdac307607d82e3644bf23859855ae2
Chris <chriscave89@gmail.com>
2023-01-30 18:36:29 +0100

    add scorecard app

---
+++ scorecard/src/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function Ball(props) {
+  return <div>{props.value}</div>;
+}
+
+class Over extends React.Component {
+  renderRow(i) {
+    let ball_numbers;
+    if (i === 0) {
+      ball_numbers = [0, 3];
+    } else if (i === 1) {
+      ball_numbers = [1, 4];
+    } else {
+      ball_numbers = [2, 5];
+    }
+
+    return (
+      <>
+        <div>
+          {" "}
+          <Ball value={this.props.value[ball_numbers[0]]} />
+        </div>
+        <div>
+          <Ball value={this.props.value[ball_numbers[1]]} />
+        </div>
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <div className="over">
+        {this.renderRow(0)}
+        {this.renderRow(1)}
+        {this.renderRow(2)}
+        {}
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Over value={[3, ".", 2, 3, ".", 5]} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
