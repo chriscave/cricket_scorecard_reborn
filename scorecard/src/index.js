b80cefc40755f31a91cbe0e6e9d372c5502f2631
Chris <chriscave89@gmail.com>
2023-02-07 22:28:10 +0100

    start trying out some layouts

--- scorecard/src/index.js
+++ scorecard/src/index.js
@@ -3,41 +3,56 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 function Ball(props) {
-  return <div> {props.value}</div>;
+  return <span className="ball"> {props.value}</span>;
 }
 
 class Over extends React.Component {
-  renderRow(i) {
-    let ball_numbers;
-    if (i === 0) {
-      ball_numbers = [0, 3];
-    } else if (i === 1) {
-      ball_numbers = [1, 4];
-    } else {
-      ball_numbers = [2, 5];
-    }
+  // renderRow(i) {
+  //   let ball_numbers;
+  //   if (i === 0) {
+  //     ball_numbers = [0, 3];
+  //   } else if (i === 1) {
+  //     ball_numbers = [1, 4];
+  //   } else {
+  //     ball_numbers = [2, 5];
+  //   }
 
-    return (
-      <>
-        <div>
-          {" "}
-          <Ball value={this.props.value[ball_numbers[0]]} />
-        </div>
-        <div>
-          <Ball value={this.props.value[ball_numbers[1]]} />
-        </div>
-      </>
-    );
-  }
+  //   return (
+  //     <>
+  //       <div>
+  //         {" "}
+  //         <Ball value={this.props.value[ball_numbers[0]]} />
+  //       </div>
+  //       <div>
+  //         <Ball value={this.props.value[ball_numbers[1]]} />
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   render() {
     return (
       <div className="over">
-        {this.renderRow(0)}
-        {this.renderRow(1)}
-        {this.renderRow(2)}
-        <div className="over-score">score</div>
+        {" "}
+        <div>
+          <Ball value={1} />
+          <Ball value={2} />
+        </div>
+        <div>
+          <Ball value={1} />
+          <Ball value={2} />
+        </div>
+        <div>
+          <Ball value={1} />
+          <Ball value={2} />
+        </div>
       </div>
+      // <div className="over">
+      //   {this.renderRow(0)}
+      //   {this.renderRow(1)}
+      //   {this.renderRow(2)}
+      //   <div className="over-score">score</div>
+      // </div>
     );
   }
 }
@@ -45,14 +60,7 @@ class Over extends React.Component {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="wrapper">
-      <div className="first">
-        <Over value={[3, ".", 2, 3, ".", 5]} />
-      </div>
-      <div className="second">
-        <Over value={[3, ".", 2, 3, ".", 5]} />
-      </div>
-    </div>
+    <Over value={[3, ".", 2, 3, ".", 5]} />
   </React.StrictMode>
 );
 
