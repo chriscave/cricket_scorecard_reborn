307942ee5b037c803063a4004bdc66794d4c9861
Chris <chriscave89@gmail.com>
2023-02-22 13:30:01 +0100

    refactor Ball detail input

--- scorecard/src/App.js
+++ scorecard/src/App.js
@@ -2,42 +2,10 @@ import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import BowlingScorecard from "./BowlingScorecard";
+import BallDetailInput from "./BallDetailInput";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-class BallDetailInput extends React.Component {
-  renderButton(i) {
-    return <button onClick={() => this.props.onClick(i)}>{i}</button>;
-  }
-  render() {
-    if (this.props.bowlerChosen) {
-      return (
-        <div>
-          <div>
-            {this.renderButton(0)}
-            {this.renderButton(1)}
-            {this.renderButton(2)}
-          </div>
-          <div>
-            {this.renderButton(3)}
-            {this.renderButton(4)}
-            {this.renderButton(6)}
-          </div>
-          <div>
-            {this.renderButton("Wkt")}
-            {/* <button>NB</button> */}
-            {/* <button>Bye</button> */}
-            {/* <button>Wide</button> */}
-            {/* <button>LB</button> */}
-          </div>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-}
 
 function BowlerNameDropdown(props) {
   if (props.bowlerChosen) {
