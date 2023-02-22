628985a620d7bc932d6c1fc96d8dca0d8014cd22
Chris <chriscave89@gmail.com>
2023-02-22 13:32:27 +0100

    refactor BowlerNamedropdown

--- scorecard/src/App.js
+++ scorecard/src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import BowlingScorecard from "./BowlingScorecard";
 import BallDetailInput from "./BallDetailInput";
+import BowlerNameDropdown from "./BowlerNameDropdown";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-function BowlerNameDropdown(props) {
-  if (props.bowlerChosen) {
-    return <div></div>;
-  } else {
-    return (
-      <DropdownButton id="bowlerNameDropdown" title="Choose a Bowler">
-        {props.names.map((name) => (
-          <Dropdown.Item key={name} onClick={() => props.onClick(name)}>
-            {name}
-          </Dropdown.Item>
-        ))}
-      </DropdownButton>
-    );
-  }
-}
 
 export default class Scorecard extends React.Component {
   constructor(props) {
