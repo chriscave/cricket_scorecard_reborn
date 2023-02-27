21077144b2a83752b87af39751beede648784d6c
Chris <chriscave89@gmail.com>
2023-02-27 11:47:57 +0100

    add BowlerName dropdown story

---
+++ scorecard/src/stories/BowlerNameDropdown.stories.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import BowlerNameDropdown from "../BowlerNameDropdown";
+import "../styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  title: "BowlerNameDropdown",
+  component: BowlerNameDropdown,
+};
+
+const Template = (args) => <BowlerNameDropdown {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  bowlerChosen: false,
+  names: ["Bowler 1", "Bowler 2"],
+  onClick: () => null,
+};
