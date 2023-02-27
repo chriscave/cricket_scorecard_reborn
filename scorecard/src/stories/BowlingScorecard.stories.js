6b99d38fd2a9702c93192aad0ece07d30f1bc559
Chris <chriscave89@gmail.com>
2023-02-27 13:30:04 +0100

    change scorecard into flex

--- scorecard/src/stories/BowlingScorecard.stories.js
+++ scorecard/src/stories/BowlingScorecard.stories.js
@@ -13,6 +13,6 @@ const Template = (args) => <BowlingScorecard {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   value: [[]],
-  names: ["Bowler1", "Bowler2"],
+  names: ["Bowler1", "Bowler2", "Bowler3", "Bowler4", "Bowler5", "Bowler6"],
   order: [],
 };
