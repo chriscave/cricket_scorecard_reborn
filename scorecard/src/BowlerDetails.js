1d6d4af005c89f3a2dc6498382bbd04ccfc0a1d5
Chris <chriscave89@gmail.com>
2023-03-03 19:45:49 +0100

    change number of overs

--- scorecard/src/BowlerDetails.js
+++ scorecard/src/BowlerDetails.js
@@ -21,7 +21,7 @@ export default function BowlerDetails(props) {
     over.reduce((a, b) => a + b)
   );
   const cumSumRuns = cumulativeSum(overRuns);
-  const noOvers = 1;
+  const noOvers = 20;
   let overNumbers = [];
   for (let i = 0; i < noOvers; i++) {
     overNumbers.push(i);
